8327586e146607e74c7607ac42da904b8fb8b684
John DeJesus <j.dejesus22@gmail.com>
2018-04-29 21:33:36 -0400

    Continue Refining Code to Classes

--- Twitter_Bot/MOMTweetTesting.py
+++ Twitter_Bot/MOMTweetTesting.py
@@ -16,7 +16,7 @@ os.chdir('C:\\Users\\Sabal\\Desktop\\Data Science Class Files\\Random Practice\\
 #from RefinedMOMTweetRetrieverCode import collectTweets
 from credentials import *
 from tweepy import TweepError
-from RefinedMOMTweetRetrieverCode import collectTweets, GeocodeDF
+from RefinedMOMTweetRetrieverCode import TweetCollector as TC
 
 #Keys to initiate connection to twitter api for bot
 consumer_key=	consumer_key
@@ -29,23 +29,14 @@ api=tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
 
 
-collected_tweets= collectTweets('2018-04-26','2018-04-27')
-collected_tweets['ID']=collected_tweets['ID'].astype('str')
-collected_tweets=GeocodeDF(collected_tweets)
-
-#pull tweets
-tweetjsons=[]
-for tweet in tweepy.Cursor(api.search,q='#makeovermonday',since='2018-04-26', until='2018-04-28',
-                           include_entities=True,result_type='recent'
-                           ,exlude_replies=True).items():
-    if 'RT' not in tweet.text:
-        tweetjsons.append(tweet)
+collected_tweets= TC.collectandStore('2018-04-26','2018-04-30')
+collected_tweets=TC.GeocodeDF(collected_tweets)
 
 
 #retweet remaining tweets
 for tweet in tweetjsons:
     try:
-        if tweet.author.name in realtweets['Name'].unique().tolist():
+        if tweet.author.name in realtweets['ID']:
             api.retweet(tweet.id)
     except TweepError:
         pass
